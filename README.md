dac7a2d323317e8f9936b764a0b10d16138acbc3
Vladimir Iglovikov <iglovikov@gmail.com>
2022-08-24 11:10:25 -0700

    Improved readme

--- README.md
+++ README.md
@@ -1,33 +1,35 @@
 # Library for converting RGB / Grayscale numpy images from to base64 and back.
 
 ## Installation
-```
+```bash
 pip install -U image_to_base_64
 ```
 
 ## Conversion
 
 ### RGB to base 64
-```
+```python
 base64 = rgb2base64(rgb_image, image_format)
 ```
 where image format is `JPEG`, `PNG`
 
 ### Grayscale to base 64
 
-```
+```python
 base64 = grayscale2base64(grayscale_image)
 ```
 
 ### Base64 to RGB image
 
-```
-rgb_image = base64_to_rgb(base64)
+```python
+output_type = "cv2" # or "PIL"
+rgb_image = base64_to_rgb(base64, output_type)
 ```
 
 ### Base64 to Grayscale image
-```
-grayscale_image = base64_to_grayscale(base64)
+```python
+output_type = "cv2" # or "PIL"
+grayscale_image = base64_to_grayscale(base64, output_type)
 ```
 
 
--- tests/test_converters.py
+++ tests/test_converters.py
@@ -5,23 +5,46 @@ from image2base64.converters import base64_to_grayscale, base64_to_rgb, grayscal
 
 
 @pytest.mark.parametrize("image_format", ["PNG"])
-def test_rgb(image_format):
+def test_rgb_cv2(image_format):
     rgb_image = np.random.randint(0, 255, (13, 79, 3), dtype=np.uint8)
     base64 = rgb2base64(rgb_image, image_format)
 
-    converted_image = base64_to_rgb(base64)
+    converted_image = base64_to_rgb(base64, output_type="cv2")
 
     assert converted_image.shape == rgb_image.shape
 
     assert np.allclose(converted_image.flatten(), rgb_image.flatten()), (converted_image - rgb_image).mean()
 
 
-def test_grayscale():
+@pytest.mark.parametrize("image_format", ["PNG"])
+def test_rgb_pil(image_format):
+    rgb_image = np.random.randint(0, 255, (13, 79, 3), dtype=np.uint8)
+    base64 = rgb2base64(rgb_image, image_format)
+
+    converted_image = base64_to_rgb(base64, output_type="PIL")
+
+    assert converted_image.size == rgb_image.shape[:2][::-1]
+
+    assert np.allclose(np.array(converted_image).flatten(), rgb_image.flatten()), (converted_image - rgb_image).mean()
+
+
+def test_grayscale_cv2():
     grayscale_image = np.random.randint(0, 255, (13, 79), dtype=np.uint8)
 
     base64 = grayscale2base64(grayscale_image)
-    converted_image = base64_to_grayscale(base64)
+    converted_image = base64_to_grayscale(base64, output_type="cv2")
 
     assert converted_image.shape == grayscale_image.shape
 
     assert np.allclose(converted_image.flatten(), grayscale_image.flatten())
+
+
+def test_grayscale():
+    grayscale_image = np.random.randint(0, 255, (13, 79), dtype=np.uint8)
+
+    base64 = grayscale2base64(grayscale_image)
+    converted_image = base64_to_grayscale(base64, output_type="PIL")
+
+    assert converted_image.size == grayscale_image.shape[::-1]
+
+    assert np.allclose(np.array(converted_image).flatten(), grayscale_image.flatten())
